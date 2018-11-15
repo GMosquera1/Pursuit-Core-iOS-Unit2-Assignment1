28e532b294a4bac599c29aa216c524d1f6c31bac
GMosquera1 <genesismosquera@pursuit.org>
2018-11-14 19:00:54 -0500

    woohoo

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -9,5 +9,16 @@
 import Foundation
 
 class TicTacToeBrain {
-  // this is where you will model your logic
+    static var gameGrid: [[Piece]] = Array(repeating: Array(repeating: Piece.empty, count: 3), count: 3)
+}
+
+struct Player {
+    let name: String
+    let piece: Piece
+    
+}
+
+enum Piece: String {
+    case x, o
+    case empty
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var firstButton: GameButton!
+    let player1 = Player(name: "Player 1", piece: .x)
+    let player2 = Player(name: "Player 2", piece: .o)
     
+    var playerToggle = true
+    
+    
+    @IBOutlet weak var firstButton: GameButton!
+    @IBOutlet weak var playerLabel: UILabel!
+    @IBAction func AllButtons(_ sender: GameButton) {
+        let currentPlayer = playerToggle ? player1: player2
+        
+        
+        switch currentPlayer.piece {
+        case .o:
+            sender.setImage(UIImage(named:"android")!, for: .normal)
+             playerLabel.text = "Player 1 is up"
+        case .x:
+            sender.setImage(UIImage(named:"swift")!, for: .normal)
+            playerLabel.text = "Player 2 is up"
+        case .empty:
+            return
+        }
+       
+        addPiece(sender: sender, piece: currentPlayer.piece)
+          print(TicTacToeBrain.gameGrid)
+        
+        sender.isUserInteractionEnabled = false
+        playerToggle = !playerToggle
+        
+    }
     
     
   override func viewDidLoad() {
     super.viewDidLoad()
-    firstButton.row = 0
-    // Do any additional setup after loading the view, typically from a nib.
+    title = "Tic Tac Toe"
+    
   }
 
+    func addPiece(sender: GameButton, piece: Piece){
+        TicTacToeBrain.gameGrid[sender.row][sender.col] = piece
+        
+    }
 
+    @IBAction func restartGame(_ sender: UIButton) {
+        sender.isUserInteractionEnabled = true
+        playerToggle = !playerToggle    }
 }
 
